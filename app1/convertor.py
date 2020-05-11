f9121fe6be8dd7580ad1f49995f1464f0b228af9
Anupriya Nishad <40591571+Anupriya1729@users.noreply.github.com>
2020-05-11 15:20:34 +0530

    Add files via upload

---
+++ app1/convertor.py
@@ -0,0 +1,88 @@
+import docx2txt
+from io import StringIO
+from pdfminer3.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer3.converter import HTMLConverter, TextConverter, XMLConverter
+from pdfminer3.layout import LAParams
+from pdfminer3.pdfpage import PDFPage
+import io
+import os
+import sys
+import getopt
+
+
+
+
+def convert(fname, pages=None):
+        if not pages:
+                pagenums = set()
+        else:
+                pagenums = set(pages)
+        manager = PDFResourceManager()
+        codec = 'utf-8'
+        caching = True
+        output = io.StringIO()
+        converter = TextConverter(
+            manager, output, codec=codec, laparams=LAParams())
+
+        interpreter = PDFPageInterpreter(manager, converter)
+        infile = open(fname, 'rb')
+
+        for page in PDFPage.get_pages(infile, pagenums, caching=caching, check_extractable=True):
+          interpreter.process_page(page)
+
+        convertedPDF = output.getvalue()
+
+        infile.close()
+        converter.close()
+        output.close()
+        return convertedPDF
+
+
+def convert_pdf_to_txt(path_to_file):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    co = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=co,laparams=laparams)
+    path_=os.getcwd()+path_to_file
+    print(path_)
+    fp = open(path_, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos = set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching, check_extractable=True):
+          interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+
+    FilePDF = 'C:/Users/admin/Desktop/Projects done/Resume Reader/Resume_Reader/media'
+    FileTXT = 'C:/Users/admin/Desktop/Projects done/Resume Reader/Resume_Reader/media/textfiles'
+    
+    base= os.path.basename(path_to_file)
+    fileNAME = os.path.splitext(base)[0]
+    fileTXT = FileTXT+'/'+fileNAME+'.txt'
+    filePDF = FilePDF+'/'+fileNAME+'.pdf'
+
+    print(filePDF)
+    convertedPDF = convert(filePDF, pages=None)
+    fileConverted = open(fileTXT, 'w+', encoding="utf-8")
+  ######## EITHER
+    fileConverted.write(convertedPDF)
+    fileConverted.close()
+    return text
+  #print(convertedPDF)
+
+  ######## OR
+  #convertedPDF=convert_pdf_to_txt(filePDF)
+  #fileConverted = open(fileTXT, "w", encoding="utf-8")
+  #fileConverted.write(convertedPDF)
+  #fileConverted.close()
+
+
